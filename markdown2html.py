20b8ca9dc5cc127a0645172e8a972a69c40beaa9
chigari101 <chigari101@gmail.com>
2023-07-16 07:23:48 -0700

    fixed file

--- markdown2html.py
+++ markdown2html.py
@@ -27,3 +27,240 @@ def markdown2html(markdown_file, output_file):
 # Execute the script
 if __name__ == "__main__":
     markdown2html(sys.argv[1], sys.argv[2])
+
+import sys
+import os
+import markdown2
+
+def markdown2html(markdown_file, output_file):
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
+
+    # Get the file paths from the command line arguments
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Check if the Markdown file exists
+    if not os.path.isfile(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    # Read the Markdown file
+    with open(markdown_file, 'r') as file:
+        markdown_text = file.read()
+
+    # Convert Markdown to HTML
+    html = markdown2.markdown(markdown_text)
+
+    # Write the HTML to the output file
+    with open(output_file, 'w') as file:
+        file.write(html)
+
+    # If everything is successful, exit with code 0
+    sys.exit(0)
+
+# Execute the script
+if __name__ == "__main__":
+    markdown2html(sys.argv[1], sys.argv[2])
+
+import sys
+import os
+import markdown2
+
+def markdown2html(markdown_file, output_file):
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
+
+    # Get the file paths from the command line arguments
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Check if the Markdown file exists
+    if not os.path.isfile(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    # Read the Markdown file
+    with open(markdown_file, 'r') as file:
+        markdown_text = file.read()
+
+    # Convert Markdown to HTML
+    html = markdown2.markdown(markdown_text, extras=['ul', 'ol'])
+
+    # Write the HTML to the output file
+    with open(output_file, 'w') as file:
+        file.write(html)
+
+    # If everything is successful, exit with code 0
+    sys.exit(0)
+
+# Execute the script
+if __name__ == "__main__":
+    markdown2html(sys.argv[1], sys.argv[2])
+
+
+import sys
+import os
+import markdown2
+
+def markdown2html(markdown_file, output_file):
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
+
+    # Get the file paths from the command line arguments
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Check if the Markdown file exists
+    if not os.path.isfile(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    # Read the Markdown file
+    with open(markdown_file, 'r') as file:
+        markdown_text = file.read()
+
+    # Convert Markdown to HTML
+    html = markdown2.markdown(markdown_text, extras=['ul', 'ol'])
+
+    # Write the HTML to the output file
+    with open(output_file, 'w') as file:
+        file.write(html)
+
+    # If everything is successful, exit with code 0
+    sys.exit(0)
+
+# Execute the script
+if __name__ == "__main__":
+    markdown2html(sys.argv[1], sys.argv[2])
+
+import sys
+import os
+import markdown2
+
+def markdown2html(markdown_file, output_file):
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
+
+    # Get the file paths from the command line arguments
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Check if the Markdown file exists
+    if not os.path.isfile(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    # Read the Markdown file
+    with open(markdown_file, 'r') as file:
+        markdown_text = file.read()
+
+    # Convert Markdown to HTML
+    html = markdown2.markdown(markdown_text, extras=['ul', 'ol', 'p'])
+
+    # Write the HTML to the output file
+    with open(output_file, 'w') as file:
+        file.write(html)
+
+    # If everything is successful, exit with code 0
+    sys.exit(0)
+
+# Execute the script
+if __name__ == "__main__":
+    markdown2html(sys.argv[1], sys.argv[2])
+
+
+import sys
+import os
+import markdown2
+
+def markdown2html(markdown_file, output_file):
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
+
+    # Get the file paths from the command line arguments
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Check if the Markdown file exists
+    if not os.path.isfile(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    # Read the Markdown file
+    with open(markdown_file, 'r') as file:
+        markdown_text = file.read()
+
+    # Convert Markdown to HTML
+    html = markdown2.markdown(markdown_text, extras=['ul', 'ol', 'p', 'b'])
+
+    # Write the HTML to the output file
+    with open(output_file, 'w') as file:
+        file.write(html)
+
+    # If everything is successful, exit with code 0
+    sys.exit(0)
+
+# Execute the script
+if __name__ == "__main__":
+    markdown2html(sys.argv[1], sys.argv[2])
+
+import sys
+import os
+import markdown2
+import re
+import hashlib
+
+
+def markdown2html(markdown_file, output_file):
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
+
+    # Get the file paths from the command line arguments
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Check if the Markdown file exists
+    if not os.path.isfile(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    # Read the Markdown file
+    with open(markdown_file, 'r') as file:
+        markdown_text = file.read()
+
+    # Convert the specified syntax to HTML
+    html_text = markdown_text
+
+    # Convert [[Hello]] to MD5 (lowercase) of the content
+    html_text = re.sub(r'\[\[(.*?)\]\]', lambda match: hashlib.md5(match.group(1).encode()).hexdigest(), html_text)
+
+    # Remove all 'c' (case insensitive) from ((Hello Chicago))
+    html_text = re.sub(r'\(\((.*?)\)\)', lambda match: match.group(1).replace('c', ''), html_text, flags=re.IGNORECASE)
+
+    # Convert Markdown to HTML for the remaining content
+    html_text = markdown2.markdown(html_text, extras=['ul', 'ol', 'p', 'b'])
+
+    # Write the HTML to the output file
+    with open(output_file, 'w') as file:
+        file.write(html_text)
+
+    # If everything is successful, exit with code 0
+    sys.exit(0)
+
+
+# Execute the script
+if __name__ == "__main__":
+    markdown2html(sys.argv[1], sys.argv[2])
